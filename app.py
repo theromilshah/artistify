4d72b419adb20e9dfc8af61164b248ae6a824193
romilshah525 <romilshah525@gmail.com>
2020-06-28 16:22:33 +0530

    Class based approach, with minor other changes

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-import os
-from datetime import datetime
+from os.path import join as create_path
 
 from flask import Flask, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
 from flask_socketio import SocketIO, send, emit
-from flask_material import Material
 
-
-from nst import training_loop
+from nst import NeuralStyleTransfer
 
 UPLOAD_FOLDER = 'static'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-Material(app)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -23,33 +19,38 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @socketio.on('message')
-def handle_message(curr, total):
+def handle_message(curr):
     socketio.send(curr)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        content, style, iterations = request.files['content'], request.files['style'], int(request.form["iterations"])
+        
+        content, style = request.files['content'], request.files['style']
+        iterations, lr, alpha, beta = int(request.form["iterations"]), float(request.form["lr"]), int(request.form["alpha"]), int(request.form["beta"])
+        
         if content.filename == '' or style.filename == "":
             return redirect(request.url)
+        
         if allowed_file(content.filename) and allowed_file(style.filename):
+            
             content_filename = secure_filename(content.filename)
-            content_filepath = os.path.join(app.config['UPLOAD_FOLDER'], content_filename)
+            content_filepath = create_path(app.config['UPLOAD_FOLDER'], content_filename)
             content.save(content_filepath)
+            
             style_filename = secure_filename(style.filename)
-            style_filepath = os.path.join(app.config['UPLOAD_FOLDER'], style_filename)
+            style_filepath = create_path(app.config['UPLOAD_FOLDER'], style_filename)
             style.save(style_filepath)
-            name = ""
-            for iteration in training_loop(content_filepath, style_filepath, iterations=iterations):
-                print("<<<", iteration)
-                if isinstance(iteration, int): handle_message(iteration+1, iterations)
-                name = iteration
-        return render_template("output.html", content_path=url_for(app.config['UPLOAD_FOLDER'], filename=content_filename),
-                                                style_path=url_for(app.config['UPLOAD_FOLDER'], filename=style_filename),
-                                                generated_path=url_for("static", filename=name))
+            
+            nst = NeuralStyleTransfer(content_filepath, style_filepath)
+            nst.set_paramers_and_hyper_parameters(iterations, alpha=alpha, beta=beta, lr=lr)
+            for val, is_training in nst.train():
+                if is_training: handle_message(val+1)
+                else: fname = nst.save_image(val)
+            return render_template("output.html", content_path=url_for(app.config['UPLOAD_FOLDER'], filename=content_filename),
+                                                    style_path=url_for(app.config['UPLOAD_FOLDER'], filename=style_filename),
+                                                    generated_path=url_for("static", filename=fname))
     else:
         return render_template("input.html")
 
--- nst.py
+++ nst.py
@@ -1,118 +1,111 @@
-import time
-import os
 from numpy import expand_dims, clip, squeeze
 from PIL.Image import fromarray
+from random import randint
 
 import tensorflow as tf
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.applications.vgg19 import VGG19
 from tensorflow.python.keras.applications.vgg19 import preprocess_input
 from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
-from random import randint
 
+class NeuralStyleTransfer(object):
 
-model = VGG19(include_top=False, weights='imagenet')
-model.trainable = False
+    def __init__(self, content_img, style_img):
+        self.content_img = self.load_and_process_image(content_img)
+        self.style_img = self.load_and_process_image(style_img)
+        self.generated_images = []
+        self.best_img = None
+        self.initialize_models_and_layers()
+        self.set_paramers_and_hyper_parameters()
 
-def load_and_process_image(image_path):
-    img = load_img(image_path, target_size = (224, 224))
-    img = img_to_array(img)
-    img = preprocess_input(img)
-    img = expand_dims(img, axis=0)
-    return img
+    def set_paramers_and_hyper_parameters(self, iterations=2, alpha=10., beta=20., lr=0.01):
+        self.iterations = iterations
+        self.alpha = alpha
+        self.beta = beta
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+        self.lam = 1. / len(self.style_models)
 
-def deprocess(x):
-    x[:, :, 0] += 103.939
-    x[:, :, 1] += 116.779
-    x[:, :, 2] += 123.68
-    x = x[:, :, ::-1]
-    x = clip(x, 0, 255).astype('uint8')
-    return x
+    def initialize_models_and_layers(self):
+        self.model = VGG19(include_top=False, weights='imagenet')
+        self.model.trainable = False
+        # self.style_layers = ['block1_conv1', 'block3_conv1', 'block5_conv1']
+        self.style_layers = ['block5_conv1']
+        self.content_layer = 'block5_conv2'
+        self.content_model = Model(inputs=self.model.input,
+                                   outputs=self.model.get_layer(self.content_layer).output)
+        self.style_models = [Model(inputs=self.model.input,
+                                   outputs=self.model.get_layer(layer).output) for layer in self.style_layers]
 
+    def load_and_process_image(self, image_path):
+        img = load_img(image_path, target_size=(224, 224))
+        img = img_to_array(img)
+        img = preprocess_input(img)
+        img = expand_dims(img, axis=0)
+        return img
 
-def save_image(image):
-    if len(image.shape) == 4:
-        img = squeeze(image, axis = 0)
+    def deprocess(self, x):
+        x[:, :, 0] += 103.939
+        x[:, :, 1] += 116.779
+        x[:, :, 2] += 123.68
+        x = x[:, :, ::-1]
+        x = clip(x, 0, 255).astype('uint8')
+        return x
 
-    img = deprocess(img)
-    im = fromarray(img)
-    filename = "final" + str(randint(10001, 999999)) + ".png"
-    im.save('./static/'+filename)
-    return filename
+    def save_image(self, image):
+        if len(image.shape) == 4:
+            img = squeeze(image, axis=0)
+        img = self.deprocess(img)
+        im = fromarray(img)
+        filename = "final" + str(randint(10001, 999999)) + ".png"
+        im.save('./static/'+filename)
+        return filename
 
+    def content_cost(self, content, generated):
+        a_C = self.content_model(content)
+        a_G = self.content_model(generated)
+        cost = tf.reduce_mean(tf.square(a_C - a_G))
+        return cost
 
-style_layers = ['block1_conv1',  'block3_conv1', 'block5_conv1']
-content_layer = 'block5_conv2'
+    def calculate_gram_matrix(self, A):
+        channels = int(A.shape[-1])
+        a = tf.reshape(A, [-1, channels])
+        n = tf.shape(a)[0]
+        gram = tf.matmul(a, a, transpose_a=True)
+        return gram / tf.cast(n, tf.float32)
 
-content_model = Model(inputs=model.input,
-                      outputs=model.get_layer(content_layer).output)
+    def style_cost(self, style, generated):
+        J_style = 0
+        for style_model in self.style_models:
+            a_S = style_model(style)
+            a_G = style_model(generated)
+            GS = self.calculate_gram_matrix(a_S)
+            GG = self.calculate_gram_matrix(a_G)
+            current_cost = tf.reduce_mean(tf.square(GS - GG))
+            J_style += current_cost * self.lam
+        return J_style
 
-style_models = [Model(inputs=model.input, outputs=model.get_layer(
-    layer).output) for layer in style_layers]
+    def train(self):
+        
+        generated = tf.Variable(self.content_img, dtype=tf.float32)
+        best_cost = 1e12+0.1
+        is_training = True
+        
+        for i in range(self.iterations):
 
-def content_cost(content, generated):
-    a_C = content_model(content)
-    a_G = content_model(generated)
-    cost = tf.reduce_mean(tf.square(a_C - a_G))
-    return cost
+            with tf.GradientTape() as tape:
+                J_content = self.content_cost(self.content_img, generated)
+                J_style = self.style_cost(self.style_img, generated)
+                J_total = self.alpha * J_content + self.beta * J_style
 
-def gram_matrix(A):
-    channels = int(A.shape[-1])
-    a = tf.reshape(A, [-1, channels])
-    n = tf.shape(a)[0]
-    gram = tf.matmul(a, a, transpose_a=True)
-    return gram / tf.cast(n, tf.float32)
+            grads = tape.gradient(J_total, generated)
+            self.optimizer.apply_gradients([(grads, generated)])
 
-lam = 1. / len(style_models)
+            if J_total < best_cost:
+                best_cost = J_total
+                self.best_image = generated.numpy()
 
-def style_cost(style, generated):
-    J_style = 0
-    for style_model in style_models:
-        a_S = style_model(style)
-        a_G = style_model(generated)
-        GS = gram_matrix(a_S)
-        GG = gram_matrix(a_G)
-        current_cost = tf.reduce_mean(tf.square(GS - GG))
-        J_style += current_cost * lam
-    return J_style
-
-generated_images = []
-
-def training_loop(content_path, style_path, iterations=20, a=10., b=20.):
-
-    content = load_and_process_image(content_path)
-    style = load_and_process_image(style_path)
-    generated = tf.Variable(content, dtype=tf.float32)
-
-    opt = tf.keras.optimizers.Adam(learning_rate=7.)
-    # opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
-
-    best_cost = 1e12+0.1
-    best_image = None
-
-    start_time = time.time()
-
-    for i in range(iterations):
-
-        with tf.GradientTape() as tape:
-            J_content = content_cost(content, generated)
-            J_style = style_cost(style, generated)
-            J_total = a * J_content + b * J_style
-
-        grads = tape.gradient(J_total, generated)
-        opt.apply_gradients([(grads, generated)])
-
-        if J_total < best_cost:
-            best_cost = J_total
-            best_image = generated.numpy()
-
-        # if i % int(iterations/10) == 0:
-        time_taken = time.time() - start_time
-        print('>>> Cost at {}: {}. Time elapsed: {}'.format(i, J_total, time_taken))
-        generated_images.append(generated.numpy())
-        # yield ('Cost at {}: {}. Time elapsed: {}'.format(i, J_total, time_taken))
-        yield i
-    
-    path = save_image(best_image)
-    print(">>>", path)
-    yield (path)
+            self.generated_images.append(generated.numpy())
+            yield i, is_training
+            
+        is_training = False
+        yield self.best_image, is_training
